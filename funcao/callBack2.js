963944372ff8a2693d8fd94e8d8fda9ee40002af
Carol Manoel <carolinamanoel37@gmail.com>
2019-07-05 18:28:34 -0300

    add: tiposDeclaracao

--- funcao/callBack2.js
+++ funcao/callBack2.js
@@ -1,17 +1,21 @@
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
 
 //Sem callback
-let notasBaixas = []
+const notasBaixas1 = []
 for (let i in notas) {
     if (notas[i] < 7) {
-        notasBaixas.push(notas[i])
+        notasBaixas1.push(notas[i])
     }
 }
 
-console.log(notasBaixas)
+console.log(notasBaixas1)
 
 //Com callback
-notasBaixas = notas.filter() 
+const notasBaixas2 = notas.filter(function (nota) {
+    return nota < 7
+})
 //É uma função que vai filtrar os elementos de um array em cima de um determinado critério. 
 //Se a função retornar true, significa que aquele elemento deve ser adicionado no array que está sendo gerado.
 //Se for false, elemento não será adicionado.
+
+console.log(notasBaixas2)
---
+++ funcao/tiposDeclaracao.js
@@ -0,0 +1,17 @@
+//Function declaration
+function soma(x, y) {
+    return x + y
+}
+
+//Function expression
+const sub = function (x, y) {
+    return x - y
+}
+
+//Named function expression
+const mult = function mult (x, y) {
+    return x * y
+}
+
+//Expression: Tem que chamar a função após a declaração.
+//Function declaration: Não precisa chamar.
