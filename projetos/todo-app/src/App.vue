51fca20f8c706261885f58cee8b4a7aafa932c98
you@example.com <you@example.com>
2019-08-24 22:49:02 -0300

    add: todo

---
+++ projetos/todo-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ToDo/>
+  </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ToDo
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  padding: 0px;
+  margin: 0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* padding-top: 60px; */
+  height: 100%;
+}
+</style>
---
+++ projetos/todo-app/src/components/ToDo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="to-do">
+    <div>
+      <input type="text" v-model="value">
+      <button @click="addItem()">Adicionar</button>
+      <br><br>
+      <div class="list">
+        <div class="item" v-for="(item, index) in list" :key="index">
+          <input type="checkbox" v-model="item.checked">
+          <span :class="{ 'is-done': item.checked === true }">{{ `${index+1} - ${item.text}` }}</span>
+          <button @click="removeItem(index)">Remover</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  data: () => ({
+    value: '',
+    list: []
+  }),
+  methods: {
+    addItem() {
+      this.list.push({ text: this.value, checked: false })
+    },
+
+    removeItem(index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.to-do {
+  display: flex;
+  justify-content: center;
+}
+
+.list {
+  width: 400px;
+  border: 1px solid grey;
+  border-radius: 5px;
+}
+
+.item {
+  background-color: lavender;
+  border: 1px solid white;
+  border-radius: 5px;
+}
+
+.is-done {
+  text-decoration: line-through;
+  color: gray;
+}
+</style>
